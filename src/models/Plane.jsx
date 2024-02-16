31878b3dcb2b94c458b131b785a5e866becf8586
Jpark <web.jpark@gmail.com>
2024-02-15 20:56:26 -0700

    Implemented 3D plane's propeller rotation based on mouse movement.

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,11 +1,21 @@
-import { useGLTF } from '@react-three/drei';
+import { useEffect, useRef } from 'react';
+import { useGLTF, useAnimations } from '@react-three/drei';
 import planeScene from '../assets/3d/plane.glb';
+import { useRotation } from '../../context/RotationContext';
 
-export default function Plane({ isRotating, ...props }) {
+export default function Plane(props) {
+	const planeRef = useRef();
 	const { scene, animations } = useGLTF(planeScene);
+	const { actions } = useAnimations(animations, planeRef);
+	const { isRotating } = useRotation();
+
+	useEffect(() => {
+		if (isRotating) actions['Take 001'].play();
+		else actions['Take 001'].stop();
+	}, [actions, isRotating]);
 
 	return (
-		<mesh {...props}>
+		<mesh ref={planeRef} {...props}>
 			<primitive object={scene} />
 		</mesh>
 	);
--- src/models/PlaneBox.jsx
+++ src/models/PlaneBox.jsx
@@ -4,13 +4,20 @@ import Plane from './Plane';
 export default function PlaneBox() {
 	const sizeValue = {
 		mobileScale: [1.5, 1.5, 1.5],
-		mobilePosition: [0, -1.5, 0],
+		mobilePosition: [0, -3.2, -1],
 		mobileRotation: [0, 20, 0],
 		desktopScale: [3, 3, 3],
-		desktopPosition: [0, -4, -4],
+		desktopPosition: [0, -5.5, -4.5],
 		desktopRotation: [0, 20, 0],
 	};
-	const [planeScale, planePosition] = useObjectScreenSizer(sizeValue);
+	const [isPlaneScale, isPlanePosition, isPlaneRotation] =
+		useObjectScreenSizer(sizeValue);
 
-	return <Plane position={planePosition} scale={planeScale} />;
+	return (
+		<Plane
+			position={isPlanePosition}
+			scale={isPlaneScale}
+			rotation={isPlaneRotation}
+		/>
+	);
 }
