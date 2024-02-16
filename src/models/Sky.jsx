ec221bf566c758bd4a387f23afb6df99c88b17a3
Jpark <web.jpark@gmail.com>
2024-02-15 20:56:49 -0700

    Implemented 3D sky rotation based on mouse movement.

--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -1,11 +1,22 @@
+import { useRef } from 'react';
 import { useGLTF } from '@react-three/drei';
+import { useFrame } from '@react-three/fiber';
 import skyScene from '../assets/3d/sky.glb';
+import { useRotation } from '../../context/RotationContext';
 
 export default function Sky() {
+	const skyRef = useRef();
 	const sky = useGLTF(skyScene);
+	const { isRotating } = useRotation();
+
+	useFrame((_, deltal) => {
+		if (isRotating) {
+			skyRef.current.rotation.y += 0.5 * deltal;
+		}
+	});
 
 	return (
-		<mesh>
+		<mesh ref={skyRef}>
 			<primitive object={sky.scene} />
 		</mesh>
 	);
