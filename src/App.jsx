58e1f555b291049eb72bad2ab077a3c22acc5e73
Jpark <web.jpark@gmail.com>
2024-02-20 15:54:52 -0700

    Resolved the GitHub Pages error occurring when refreshing the page | updated the Error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,34 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Root, Home, About, Projects, Contact } from './pages/index';
 import Error from './components/Error';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Root />,
-		errorElement: <Error />,
-		children: [
-			{
-				path: '/',
-				element: <Home />,
-			},
-			{
-				path: '/about',
-				element: <About />,
-			},
-			{
-				path: '/projects',
-				element: <Projects />,
-			},
-			{
-				path: '/contact',
-				element: <Contact />,
-			},
-		],
-	},
-	{ basename: import.meta.env.DEV ? '/' : '/3d-portfolio/' },
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Root />,
+			errorElement: <Error />,
+			children: [
+				{
+					path: '/',
+					element: <Home />,
+				},
+				{
+					path: '/about',
+					element: <About />,
+				},
+				{
+					path: '/projects',
+					element: <Projects />,
+				},
+				{
+					path: '/contact',
+					element: <Contact />,
+				},
+			],
+		},
+	],
+	{ basename: import.meta.env.DEV ? '/' : '/3d-portfolio/' }
+);
 
 export default function App() {
 	return <RouterProvider router={router} />;
--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -5,10 +5,14 @@ export default function Error() {
 	const error = useRouteError();
 
 	return (
-		<div>
-			<h1>Something went wrong 😢</h1>
-			<p>{error.message || error.data}</p>
-			<button onClick={() => navigate(-1)}>&larr; Go back</button>
-		</div>
+		<section className="max-container">
+			<h1 className="head-text">Something went wrong 😢</h1>
+			<p className="my-10 text-lg text-slate-500 sm:text-2xl">
+				{error.message || error.data}
+			</p>
+			<button className="btn" onClick={() => navigate('/')}>
+				&larr; Go back to Home page
+			</button>
+		</section>
 	);
 }
