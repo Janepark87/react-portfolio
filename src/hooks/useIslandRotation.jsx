d37b65b108b00477c9a7d02253e9e6bd30223614
Jpark <web.jpark@gmail.com>
2024-02-20 13:15:09 -0700

    Adjusted the smooth rotation of the island by controlling the keyboard keys.

--- src/hooks/useIslandRotation.jsx
+++ src/hooks/useIslandRotation.jsx
@@ -46,9 +46,11 @@ export default function useIslandRotation(
 			if (e.key === 'ArrowLeft') {
 				if (!isRotating) setIsRotating(true);
 				islandRef.current.rotation.y += 0.01 * Math.PI;
+				rotationSpeed.current = 0.0125;
 			} else if (e.key === 'ArrowRight') {
 				if (!isRotating) setIsRotating(true);
 				islandRef.current.rotation.y -= 0.01 * Math.PI;
+				rotationSpeed.current = -0.0125;
 			}
 		};
 
