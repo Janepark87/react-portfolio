9eb151ccef04dca210075fd38d13e69256006549
Jpark <web.jpark@gmail.com>
2024-02-22 12:35:10 -0700

    updated the useObjectScreenSize hook

--- src/hooks/useObjectScreenSizer.jsx
+++ src/hooks/useObjectScreenSizer.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 export default function useObjectScreenSizer({
 	mobileScale = null,
 	mobilePosition = null,
@@ -6,7 +8,19 @@ export default function useObjectScreenSizer({
 	desktopPosition = null,
 	desktopRotation = null,
 }) {
-	const isMobile = window.innerWidth < 768;
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia('(max-width: 768px)');
+		setIsMobile(mediaQuery.matches);
+
+		const handleMediaQueryChange = (e) => setIsMobile(e.matches);
+
+		mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+		return () =>
+			mediaQuery.removeEventListener('change', handleMediaQueryChange);
+	}, []);
 
 	const scale = isMobile ? mobileScale : desktopScale;
 	const position = isMobile ? mobilePosition : desktopPosition;
